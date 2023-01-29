bce75362a04f1638fd99fec07c211ef027a577dd
Frazic <bruylant.joshua@gmail.com>
2023-01-29 12:49:16 +0100

    Updated server for gcloud loggin

--- node_server/supabase.ts
+++ node_server/supabase.ts
@@ -1,5 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
 module.exports = createClient(
-    process.env.APP_SUPABASE_URL,
-    process.env.APP_SUPABASE_KEY
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
 );
--- node_server/winston.ts
+++ node_server/winston.ts
@@ -5,50 +5,18 @@ const Mail = require("winston-mail").Mail;
 module.exports = winston.createLogger({
     transports: [
         new winston.transports.Console({
-            level: "info",
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json()
-            ),
-        }),
-        new winston.transports.DailyRotateFile({
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json()
-            ),
-            level: "debug",
-            filename: "log/%DATE%/debug.log",
-            auditFile: "log/auditFiles/debug.json",
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d'
-        }),
-        new winston.transports.DailyRotateFile({
             format: winston.format.combine(
                 winston.format.timestamp(),
                 winston.format.json()
             ),
             level: "info",
-            filename: "log/%DATE%/info.log",
-            auditFile: "log/auditFiles/info.json",
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d'
         }),
-        new winston.transports.DailyRotateFile({
+        new winston.transports.Console({
             format: winston.format.combine(
                 winston.format.timestamp(),
                 winston.format.json()
             ),
             level: "error",
-            filename: "log/%DATE%/error.log",
-            auditFile: "log/auditFiles/error.json",
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d'
         }),
         new winston.transports.Mail({
             format: winston.format.combine(
@@ -59,48 +27,36 @@ module.exports = winston.createLogger({
             to: "frazic.dev@gmail.com",
             from: "frazic.dev@gmail.com",
             subject: "Inclure.Net Error",
-            host: process.env.APP_SENDINBLUE_HOST,
-            port: process.env.APP_SENDINBLUE_PORT,
-            username: process.env.APP_SENDINBLUE_USER,
-            password: process.env.APP_SENDINBLUE_PASSWORD,
+            host: process.env.SENDINBLUE_HOST,
+            port: process.env.SENDINBLUE_PORT,
+            username: process.env.SENDINBLUE_USER,
+            password: process.env.SENDINBLUE_PASSWORD,
         })
     ],
     exceptionHandlers: [
-        new winston.transports.DailyRotateFile({
+        new winston.transports.Console({
             format: winston.format.combine(
                 winston.format.timestamp(),
                 winston.format.json()
             ),
-            filename: "log/%DATE%/exception.log",
-            auditFile: "log/auditFiles/exception.json",
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d'
         }),
         new winston.transports.Mail({
             to: "frazic.dev@gmail.com",
             from: "frazic.dev@gmail.com",
             subject: "Inclure.Net Exception",
-            host: process.env.APP_SENDINBLUE_HOST,
-            port: process.env.APP_SENDINBLUE_PORT,
-            username: process.env.APP_SENDINBLUE_USER,
-            password: process.env.APP_SENDINBLUE_PASSWORD,
+            host: process.env.SENDINBLUE_HOST,
+            port: process.env.SENDINBLUE_PORT,
+            username: process.env.SENDINBLUE_USER,
+            password: process.env.SENDINBLUE_PASSWORD,
             ssl: true
         })
     ],
     rejectionHandlers: [
-        new winston.transports.DailyRotateFile({
+        new winston.transports.Console({
             format: winston.format.combine(
                 winston.format.timestamp(),
                 winston.format.json()
             ),
-            filename: "log/%DATE%/rejection.log",
-            auditFile: "log/auditFiles/rejection.json",
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d'
         })
     ],
     exitOnError: false
